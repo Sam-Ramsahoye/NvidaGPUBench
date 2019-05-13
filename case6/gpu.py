1707118780699524be62cf98766f001528d1f347
Sam Ramsahoye <sam.ramsahoye@arup.com>
2019-05-13 17:15:55 +0100

    Updated!

---
+++ case6/gpu.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import os
+import pandas as pd
+from datetime import datetime
+import cudf
+from numba import cuda
+import numba
+import time
+import numpy as np
+from joblib import Parallel ,delayed
+import sys
+
+# fileRead
+def file_read(path,file,ew_gpu):
+    cudfObj = cudf.read_csv(path+file).drop(['Date','Time'])
+    return ew_gpu.merge(cudfObj, on=['Lat', 'Lon'])
+
+# Getting ID-Location Association
+ew_gpu = cudf.read_csv('../ew_rcid_lat_lon.csv', dtype = ['category','float64','float64'])
+
+path = '../../data/'
+files = os.listdir(path)
+filesRequired = [f for f in files if 'nr' in f]
+
+nProc = 16
+nFilesArr = [10]
+nFilesArr = [i*nProc for i in nFilesArr]
+dfSizeArr = []
+tArr = []
+
+for nFiles in nFilesArr:
+    start = time.time()
+    out = Parallel(n_jobs = nProc)(delayed(file_read)(path,f,ew_gpu) for f in filesRequired[:nFiles])
+    df = cudf.concat(out)
+    end = time.time()
+    print(end-start)
+    tArr.append(end-start)
+    dfSizeArr.append(sys.getsizeof(df))
+    del out
+
+    df = df.sort_values(by=['RCID','Precipitation (mm)'])
+
+#print(tArr)
+#print(dfSizeArr)
