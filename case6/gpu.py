d50336bd4cda683dc73178318b3d222633245f27
Sam Ramsahoye <sam.ramsahoye@arup.com>
2019-05-14 09:45:54 +0100

    Updated!

--- case6/gpu.py
+++ case6/gpu.py
@@ -23,7 +23,7 @@ files = os.listdir(path)
 filesRequired = [f for f in files if 'nr' in f]
 
 nProc = 16
-nFilesArr = [300]
+nFilesArr = [250]
 nFilesArr = [i*nProc for i in nFilesArr]
 dfSizeArr = []
 tArr = []
