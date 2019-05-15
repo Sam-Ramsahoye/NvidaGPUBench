ad314cfc89002993c12a4a0af1f4fcb8eb395994
Sam Ramsahoye <sam.ramsahoye@arup.com>
2019-05-15 12:35:02 +0100

    Updated!

--- case7/cpu.py
+++ case7/cpu.py
@@ -1,7 +1,8 @@
 import cudf
 import pandas as pd
 import time
-#df = cudf.read_csv('temp.csv')
+
+ew = pd.read_csv('../ew_rcid_lat_lon.csv')
 start = time.time()
 df = pd.read_csv('/home/data/temp.csv', parse_dates = ['DateTime'])
 end = time.time()
@@ -12,3 +13,5 @@ df = df[df['precip'] > 0]
 end = time.time()
 print(end-start)
 print(df.head(10))
+df = ew.merge(df, on=['Lat', 'Lon'])
+print(len(df))
--- case7/gpu.py
+++ case7/gpu.py
@@ -1,7 +1,8 @@
 import cudf
 import pandas as pd
 import time
-#df = cudf.read_csv('temp.csv')
+
+ew = cudf.read_csv('../ew_rcid_lat_lon.csv', dtype = ['category','float64','float64'])
 start = time.time()
 df = cudf.read_csv('/home/data/temp.csv',dtype = ['float32','float32','float32','date'])
 end = time.time()
@@ -12,3 +13,5 @@ df = df.query("precip > 0")
 end = time.time()
 print(end-start)
 print(df.head(10))
+df = ew.merge(df, on=['Lat', 'Lon'])
+print(len(df))
