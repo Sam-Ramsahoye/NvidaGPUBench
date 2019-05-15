ccf5e4144cf8cc498ddfd22e5623a87fb3a75d0e
Sam Ramsahoye <sam.ramsahoye@arup.com>
2019-05-15 17:14:15 +0100

    Updated!

--- case7/gpu.py
+++ case7/gpu.py
@@ -2,9 +2,9 @@ import cudf
 import pandas as pd
 import time
 
-ew = cudf.read_csv('../ew_rcid_lat_lon.csv', dtype = ['category','float64','float64'])
+ew = cudf.read_csv('../ew_rcid_lat_lon.csv', dtype = ['category','float32','float32'])
 start = time.time()
-df = cudf.read_csv('/home/data/temp.csv',dtype = ['float64','float64','float32','date'])
+df = cudf.read_csv('/home/data/temp.csv',dtype = ['float32','float32','float32','date'])
 end = time.time()
 print(end-start)
 print(len(df))
